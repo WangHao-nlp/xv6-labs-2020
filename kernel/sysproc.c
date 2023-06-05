23e30a0a089ea7b20295b0f12b38c5ef1651a5a1
gitwh230524 <wanghao1999_cs@163.com>
2023-06-05 07:17:20 +0000

    完成了第四个作业

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -58,6 +58,8 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
+  backtrace(); // new add print stack backtrace. 
+
   if(argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
@@ -95,3 +97,33 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_sigalarm(void) {
+  int n;
+  uint64 fn;
+  if(argint(0, &n) < 0)
+    return -1;
+  if(argaddr(1, &fn) < 0)
+    return -1;
+  
+  return sigalarm(n, (void(*)())(fn));
+}
+
+uint64 sys_sigreturn(void) {
+	return sigreturn();
+}
+
+//uint64 sys_sigalarm(void){
+//  int n;
+//  uint64 fn;
+//  if(argint(0, &n)<0)
+//    return -1;
+//  if(argaddr(1, &fn)<0)
+//    return -1;
+//
+//  return sigalarm(n, (void(*)())(fn));
+//}
+//
+//uint64 sys_sigreturn(void){
+//  return sigreturn();
+//}

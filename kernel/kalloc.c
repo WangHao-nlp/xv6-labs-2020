ebf02d57b7307e2598131c8841ad5047c6ec40bd
gitwh230524 <wanghao1999_cs@163.com>
2023-06-07 11:28:30 +0000

    完成了第六个作业cow

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,26 @@ struct {
   struct run *freelist;
 } kmem;
 
+// For indexing the copy-on-write page reference count array
+#define PA2PGREF_ID(p) (((p)-KERNBASE)/PGSIZE)
+#define PGREF_MAX_ENTRIES PA2PGREF_ID(PHYSTOP)
+
+struct spinlock pgreflock;
+int pageref[PGREF_MAX_ENTRIES]; // reference count for each physical page
+// note:  reference counts are incremented on fork, not on mapping. this means that
+//        multiple mappings of the same physical page within a single process are only
+//        counted as one reference.
+//        this shouldn't be a problem, though. as there's no way for a user program to map
+//        a physical page twice within it's address space in xv6.
+
+#define PA2PGREF(p) pageref[PA2PGREF_ID((uint64)(p))]
+
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&pgreflock, "pgref"); // 初始化锁
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -50,16 +66,33 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+  
+  acquire(&pgreflock);
+  if(--PA2PGREF(pa) <= 0) {
+    // 当页面的引用计数小于等于 0 的时候，释放页面
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    // Fill with junk to catch dangling refs.
+    // pa will be memset multiple times if race-condition occurred.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  }
+  release(&pgreflock);
+
+  // // Fill with junk to catch dangling refs.
+  // memset(pa, 1, PGSIZE);
+
+  // r = (struct run*)pa;
+
+  // acquire(&kmem.lock);
+  // r->next = kmem.freelist;
+  // kmem.freelist = r;
+  // release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -76,7 +109,43 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
+    // 新分配的物理页的引用计数为 1
+    // (这里无需加锁)
+    PA2PGREF(r) = 1;
+  }
   return (void*)r;
+}
+
+
+// 当引用已经小于等于 1 时，不创建和复制到新的物理页，而是直接返回该页本身
+void *kcopy_n_deref(void *pa) {
+  acquire(&pgreflock);
+
+  if(PA2PGREF(pa) <= 1) { // 只有 1 个引用，无需复制
+    release(&pgreflock);
+    return pa;
+  }
+
+  // 分配新的内存页，并复制旧页中的数据到新页
+  uint64 newpa = (uint64)kalloc();
+  if(newpa == 0) {
+    release(&pgreflock);
+    return 0; // out of memory
+  }
+  memmove((void*)newpa, (void*)pa, PGSIZE);
+
+  // 旧页的引用减 1
+  PA2PGREF(pa)--;
+
+  release(&pgreflock);
+  return (void*)newpa;
+}
+
+// 为 pa 的引用计数增加 1
+void krefpage(void *pa) {
+  acquire(&pgreflock);
+  PA2PGREF(pa)++;
+  release(&pgreflock);
 }
